21ffe4d058f80ffdb09ab9005e25316b4adeee2e
Md Shojib Hossain <mdshojib922@gmail.com>
2024-03-25 16:07:10 +0600

    Login Completed

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+let cookieParser = require('cookie-parser')
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -8,13 +9,23 @@ const bcrypt = require("bcrypt");
 const Mailgen = require("mailgen");
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "http://localhost:5173",
+    credentials: true,
+  }
+));
 app.use(express.json());
-
+app.use(cookieParser());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
+//This is for Ashik
+// const uri =
+//   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nhg2oh1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+//This is for Shojib
 const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nhg2oh1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oglq0ui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,86 +39,88 @@ async function run() {
   try {
     await client.connect();
     // Send a ping to confirm a successful connection
-      
-      const usersCollection = client.db("DNCC").collection("user");
 
-        const verifyAdmin = async (req, res, next) => {
-          const email = req.decoded.email;
-          const query = { email: email };
-          const user = await usersCollection.findOne(query);
-          const isAdmin = user?.role === "admin";
-          if (!isAdmin) {
-            return res.status(403).send({ message: "forbidden access" });
-          }
-          next();
-        };
-      
-      const verifyToken = async (req, res, next) => {
-        let token = req?.cookies?.token;
-        console.log("Value of token in middleware: ", token);
-        if (!token) {
-          return res.status(401).send({ message: "Not Authorized" });
-        }
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-          if (err) {
-            console.log(err);
-            return res.status(401).send({ message: "UnAuthorized" });
-          }
-          console.log("value in the token", decoded);
-          req.user = decoded;
-          next();
+    const usersCollection = client.db("DNCC").collection("user");
+
+    //Verify Token
+    // const verifyToken = async (req, res, next) => {
+    //   let token = req?.cookies?.token;
+    //   console.log("Value of token in middleware: ", token);
+    //   if (!token) {
+    //     return res.status(401).send({ message: "Not Authorized" });
+    //   }
+    //   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    //     if (err) {
+    //       console.log(err);
+    //       return res.status(401).send({ message: "UnAuthorized" });
+    //     }
+    //     console.log("value in the token", decoded);
+    //     req.user = decoded;
+    //     next();
+    //   });
+    // };
+
+    //Verify Admin
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email;
+    //   const query = { email: email };
+    //   const user = await usersCollection.findOne(query);
+    //   const isAdmin = user?.role === "admin";
+    //   if (!isAdmin) {
+    //     return res.status(403).send({ message: "forbidden access" });
+    //   }
+    //   next();
+    // };
+
+
+    // verifyToken, verifyAdmin,
+    app.post("/auth/create", async (req, res) => {
+      const user = req.body;
+
+      const userEmail = { email: user.email };
+      const findUser = await usersCollection.findOne(userEmail);
+      if (findUser) {
+        return res.json({ msg: `${user.email} is already registered` });
+      } else {
+        const userPassword = user.password;
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(userPassword, salt);
+        user.password = hashedPassword;
+        user.role = "unassigned";
+
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      }
+    });
+
+    //Auth Login
+    app.post("/auth/login", async (req, res) => {
+      const { email, password } = req.body;
+
+      const user = await usersCollection.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      let token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h' });
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: true,  
+          sameSite: 'none'
+        })
+        .json({
+          success: true,
+          message: "Login successful",
+          token,
         });
-      };
+    });
 
-      app.post("/auth/create", verifyToken, verifyAdmin ,async (req, res) => {
-        const user = req.body;
-
-        const userEmail = { email: user.email };
-        const findUser = await usersCollection.findOne(userEmail);
-        if (findUser) {
-          return res.json({ msg: `${user.email} is already registered` });
-        } else {
-          const userPassword = user.password;
-          const salt = await bcrypt.genSalt(10);
-          const hashedPassword = await bcrypt.hash(userPassword, salt);
-          user.password = hashedPassword;
-          user.role = "unassigned";
-
-          const result = await usersCollection.insertOne(user);
-          res.send(result);
-        }
-      });
-
-      app.post("/auth/login", async (req, res) => {
-        const { email, password } = req.body;
-
-        const user = await usersCollection.findOne({ email });
-        if (!user) {
-          return res.status(401).json({ message: "Invalid email or password" });
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-          return res.status(401).json({ message: "Invalid email or password" });
-        }
-
-        const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
-          expiresIn: process.env.EXPIRES_IN,
-        });
-
-        res
-          .cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none",
-          })
-          .json({
-            success: true,
-            message: "Login successful",
-            token,
-          });
-      });
-    
 
     await client.db("admin").command({ ping: 1 });
     console.log(
