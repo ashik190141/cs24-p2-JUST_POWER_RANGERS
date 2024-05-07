e52354bdc79affca30a36a63603ce1e134392682
Md Shojib Hossain <mdshojib922@gmail.com>
2024-05-07 20:44:13 +0600

    Title Animation Updated

--- frontend/src/Components/SectionTitle.jsx
+++ frontend/src/Components/SectionTitle.jsx
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
+import MovingText from 'react-moving-text'
 
 const SectionTitle = ({ title, subTitle }) => {
     return (
         <div className="text-center my-5 uppercase">
             <h3>{subTitle}</h3>
-            <h1 className="text-4xl font-bold text-red-900">{title}</h1>
+            <h1 className="text-4xl font-bold text-red-900">
+                <MovingText
+                    type="fadeInFromTop"
+                    duration="1000ms"
+                    delay="0s"
+                    direction="normal"
+                    timing="ease"
+                    iteration="2"
+                    fillMode="none">
+                    {title}
+                </MovingText>
+            </h1>
         </div>
     )
 }
