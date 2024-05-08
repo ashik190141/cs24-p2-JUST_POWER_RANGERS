c7e7d1e560e779f8702aee513c41e382790aebf2
Md Shojib Hossain <mdshojib922@gmail.com>
2024-05-08 23:53:10 +0600

    Counter Updated

--- frontend/src/Pages/Home/Counter.jsx
+++ frontend/src/Pages/Home/Counter.jsx
@@ -18,7 +18,7 @@ const Counter = () => {
                 className='bg-gradient-to-r from-emerald-600 to-cyan-900 my-5 py-8 mx-6 grid grid-cols-2 md:grid-cols-4 gap-10 justify-center items-center rounded-md'>
                 <div data-aos="fade-up" data-aos-duration="3000"
                     className='text-center bg-white rounded-full p-3'>
-                    <h1 className='text-2xl font-bold mb-2'>Number of Users</h1>
+                    <h1 className='text-2xl font-bold mb-2'>Number of User</h1>
                     <div className='h-12 w-12 md:h-20 md:w-20 rounded-full mx-auto bg-sky-900 text-white p-3 md:p-6'>
                         <CountUp
                             className='text-xl font-bold'
@@ -42,7 +42,7 @@ const Counter = () => {
                 </div>
                 <div data-aos="fade-up" data-aos-duration="3000"
                     className='text-center bg-white rounded-full p-3'>
-                    <h1 className='text-2xl font-bold mb-2'>Number of Landfil</h1>
+                    <h1 className='text-2xl font-bold mb-2'>Number of Landfill</h1>
                     <div className='h-12 w-12 md:h-20 md:w-20 rounded-full mx-auto bg-sky-900 text-white p-3 md:p-6'>
                         <CountUp
                             className='text-xl font-bold'
